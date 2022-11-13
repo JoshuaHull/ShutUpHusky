5c98d27735c58809317f135ae3fc0d3ac92188a8
Joshua Hull <josh.hull.w@gmail.com>
2022-11-13 19:45:02 +1100

    feat(rand-8244): updated program

--- src/ShutUpHusky/Program.cs
+++ src/ShutUpHusky/Program.cs
@@ -1,16 +1,26 @@
-﻿using LibGit2Sharp;
+﻿using System.Diagnostics;
+using LibGit2Sharp;
 using ShutUpHusky;
 
-if (args.Length < 3) {
-    Console.WriteLine(@"Expected usage: dotnet run ""Author Name"" ""author@email.com"" ""C:\Users\Author\SomeRepo""");
+if (args.Length < 1) {
+    Console.WriteLine(@"Expected usage: dotnet run ""C:\Users\User\SomeRepo""");
     return;
 }
 
-var repo = new Repository(args[2]);
-var me = new Signature(args[0], args[1], DateTimeOffset.UtcNow);
+var repo = new Repository(args[0]);
 
 var commitMessage = new CommitMessageAssembler().Assemble(repo);
 
 Console.WriteLine(commitMessage);
 
-repo.Commit(commitMessage, me, me);
+var info = new ProcessStartInfo {
+    FileName = "cmd.exe",
+    Arguments = @$"/K git commit -m ""{commitMessage}"" && exit",
+    CreateNoWindow = false,
+    UseShellExecute = false,
+    WorkingDirectory = args[0],
+};
+
+var process = Process.Start(info);
+
+process?.WaitForExit();
