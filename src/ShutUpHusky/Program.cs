c1bf7f4a57b129177bbd482cd5c7f5ebc5d81723
Joshua Hull <josh.hull.w@gmail.com>
2022-11-13 03:14:02 +0000

    feat(rand-354): created program

---
+++ src/ShutUpHusky/Program.cs
@@ -0,0 +1,16 @@
+﻿using LibGit2Sharp;
+using ShutUpHusky;
+
+if (args.Length < 3) {
+    Console.WriteLine(@"Expected usage: dotnet run ""Author Name"" ""author@email.com"" ""C:\Users\Author\SomeRepo""");
+    return;
+}
+
+var repo = new Repository(args[2]);
+var me = new Signature(args[0], args[1], DateTimeOffset.UtcNow);
+
+var commitMessage = new CommitMessageAssembler().Assemble(repo);
+
+Console.WriteLine(commitMessage);
+
+repo.Commit(commitMessage, me, me);
