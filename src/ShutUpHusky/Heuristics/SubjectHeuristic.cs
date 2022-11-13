f43e0de0e36adbfb2615607123921f2521b316ad
Joshua Hull <josh.hull.w@gmail.com>
2022-11-13 03:12:49 +0000

    feat(rand-4629): subject > created subject-heuristic-tests

---
+++ src/ShutUpHusky/Heuristics/SubjectHeuristic.cs
@@ -0,0 +1,47 @@
+using LibGit2Sharp;
+using ShutUpHusky.Utils;
+
+namespace ShutUpHusky.Heuristics;
+
+public class SubjectHeuristic : IHeuristic {
+    public HeuristicResult Analyse(IRepository repo) {
+        var stagedFiles = repo
+            .RetrieveStatus(new StatusOptions())
+            .Where(file =>
+                file.State == FileStatus.ModifiedInIndex ||
+                file.State == FileStatus.NewInIndex ||
+                file.State == FileStatus.RenamedInIndex ||
+                file.State == FileStatus.DeletedFromIndex
+            )
+            .ToList();
+
+        if (stagedFiles.Count == 0)
+            return new() {
+                Priority = 0,
+                Value = string.Empty,
+            };
+
+        var fileTerms = stagedFiles
+            .Select(f => f.FilePath.GetFileName().CamelCaseToKebabCase())
+            .SelectMany(n => n.Split("-"));
+
+        var fileTermsByCount = fileTerms
+            .GroupBy(t => t)
+            .ToDictionary(t => t.First(), t => t.Count());
+
+        var mostCommonTerm = fileTermsByCount
+            .OrderByDescending(k => k.Value)
+            .First();
+
+        if (mostCommonTerm.Value == 1)
+            return new() {
+                Priority = 0,
+                Value = string.Empty,
+            };
+
+        return new() {
+            Priority = 1,
+            Value = mostCommonTerm.Key,
+        };
+    }
+}
---
+++ tests/ShutUpHusky.UnitTests/Heuristics/SubjectHeuristicTests.cs
@@ -0,0 +1,49 @@
+using LibGit2Sharp;
+using NUnit.Framework;
+using ShutUpHusky.Heuristics;
+using ShutUpHusky.TestSdk.Mocks;
+
+namespace ShutUpHusky.UnitTests.Heuristics;
+
+public class SubjectHeuristicTests
+{
+    private SubjectHeuristic Heuristic => new();
+
+    [TestCase("programs", "entries", "programs-directory", ExpectedResult = "programs")]
+    [TestCase("cats", "dogs", "fish", ExpectedResult = "")]
+    [TestCase("really-cool-file.ts", "less-cool-file.ts", "cool.test.ts", ExpectedResult = "cool")]
+    [TestCase("ReallyCoolFile.ts", "less-cool-file.ts", "ThisFile-IsCool.test.ts", ExpectedResult = "cool")]
+    public string ShouldChooseSubjectFromFileNames_WhenThereIsACommonTerm(
+        string firstFileName, string secondFileName, string thirdFileName
+    ) {
+        // Arrange
+        var firstEntry = new MockStatusEntry {
+            FilePath = firstFileName,
+            State = FileStatus.ModifiedInIndex,
+        };
+        var secondEntry = new MockStatusEntry {
+            FilePath = secondFileName,
+            State = FileStatus.NewInIndex,
+        };
+        var thirdEntry = new MockStatusEntry {
+            FilePath = thirdFileName,
+            State = FileStatus.DeletedFromIndex,
+        };
+
+        var repo = new MockRepository {
+            Status = new MockRepositoryStatus {
+                StatusEntries = new List<StatusEntry> {
+                    firstEntry.Object,
+                    secondEntry.Object,
+                    thirdEntry.Object,
+                },
+            }.Object,
+        };
+
+        // Act
+        var result = Heuristic.Analyse(repo.Object);
+
+        // Assert
+        return result.Value;
+    }
+}
