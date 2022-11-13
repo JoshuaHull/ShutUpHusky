dba0be7a505986ca369cb7eebafe243d492ae7d8
Joshua Hull <josh.hull.w@gmail.com>
2022-11-13 03:13:04 +0000

    feat(rand-9126): deletion > created deletion-heuristic-tests

---
+++ src/ShutUpHusky/Heuristics/DeletionHeuristic.cs
@@ -0,0 +1,43 @@
+using LibGit2Sharp;
+using ShutUpHusky.Utils;
+
+namespace ShutUpHusky.Heuristics;
+
+public class DeletionHeuristic : IHeuristic {
+    public HeuristicResult Analyse(IRepository repo) {
+        var status = repo.RetrieveStatus(new StatusOptions());
+
+        var modifiedFiles = status
+            .Where(file => file.State == FileStatus.DeletedFromIndex)
+            .ToList();
+
+        if (modifiedFiles.Count == 0)
+            return new() {
+                Priority = 0,
+                Value = string.Empty,
+            };
+
+        var patches = modifiedFiles
+            .ToDictionary(file => repo.Diff.Compare<Patch>(repo.Head.Tip.Tree, DiffTargets.Index, new List<string> {
+                file.FilePath,
+            }));
+
+        var largestDeletedFile = patches
+            .Keys
+            .OrderByDescending(patch => patch.LinesDeleted)
+            .First();
+
+        if (largestDeletedFile.LinesDeleted == 0)
+            return new() {
+                Priority = 0,
+                Value = string.Empty,
+            };
+
+        var fileName = patches[largestDeletedFile].FilePath.GetFileName().CamelCaseToKebabCase();
+
+        return new() {
+            Priority = 1,
+            Value = $"deleted {fileName}",
+        };
+    }
+}
---
+++ tests/ShutUpHusky.UnitTests/Heuristics/DeletionHeuristicTests.cs
@@ -0,0 +1,68 @@
+using FluentAssertions;
+using LibGit2Sharp;
+using NUnit.Framework;
+using ShutUpHusky.Heuristics;
+using ShutUpHusky.TestSdk.Mocks;
+
+namespace ShutUpHusky.UnitTests.Heuristics;
+
+public class DeletionHeuristicTests
+{
+    private DeletionHeuristic Heuristic => new();
+
+    [Test]
+    public void ShouldReturnDeletionLabel_ForTheLargestDeletedFile()
+    {
+        // Arrange
+        var smallDeletedFile = new MockPatch {
+            LinesAdded = 0,
+            LinesDeleted = 50,
+        };
+
+        var largeDeletedFile = new MockPatch {
+            LinesAdded = 0,
+            LinesDeleted = 100,
+        };
+
+        var modifiedFile = new MockPatch {
+            LinesAdded = 10,
+            LinesDeleted = 20,
+        };
+
+        var repo = new MockRepository {
+            Head = new MockBranch {
+                Tip = new MockCommit {
+                    Tree = new MockTree {
+                    }.Object,
+                }.Object,
+            }.Object,
+            Status = new MockRepositoryStatus {
+                StatusEntries = new[] {
+                    new MockStatusEntry {
+                        State = FileStatus.DeletedFromIndex,
+                        FilePath = "files/smallDeletedFile",
+                    }.Object,
+                    new MockStatusEntry {
+                        State = FileStatus.DeletedFromIndex,
+                        FilePath = "files/largeDeletedFile",
+                    }.Object,
+                    new MockStatusEntry {
+                        State = FileStatus.ModifiedInIndex,
+                        FilePath = "files/modifiedFile",
+                    }.Object,
+                },
+            }.Object,
+            Diff = new MockDiff()
+                .SeedPatch("files/smallDeletedFile", smallDeletedFile.Object)
+                .SeedPatch("files/largeDeletedFile", largeDeletedFile.Object)
+                .SeedPatch("files/modifiedFile", modifiedFile.Object)
+                .Object,
+        };
+
+        // Act
+        var result = Heuristic.Analyse(repo.Object);
+
+        // Assert
+        result.Value.Should().Be("deleted large-deleted-file");
+    }
+}
