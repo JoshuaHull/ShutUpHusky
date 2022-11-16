429d9528125be3ef0cfa0509d68801ebbfefd1b1
Joshua Hull <josh.hull.w@gmail.com>
2022-11-17 00:00:07 +1100

    feat(rand-204): updated commit-message-assembler

--- src/ShutUpHusky/CommitMessageAssembler.cs
+++ src/ShutUpHusky/CommitMessageAssembler.cs
@@ -6,49 +6,27 @@ namespace ShutUpHusky;
 public class CommitMessageAssembler {
     private const int MaxCommitTitleLength = 72;
 
+    private static IEnumerable<IHeuristic> Heuristics => new IHeuristic[] {
+        new TicketHeuristic(),
+        new SubjectHeuristic(),
+        new CreationHeuristic(),
+        new DeletionHeuristic(),
+        new ModificationHeuristic(),
+        new RenamingHeuristic(),
+    };
+
     public string Assemble(Repository repo) {
         var result = string.Empty;
         var separator = string.Empty;
+        var heuristics = Heuristics;
 
-        if (MaybeAddTicket(repo, separator, ref result))
-            separator = ": ";
-
-        if (MaybeAddSubject(repo, separator, ref result))
-            separator = " > ";
-
-        if (MaybeAddCreation(repo, separator, ref result))
-            separator = ", ";
-
-        if (MaybeAddDeletion(repo, separator, ref result))
-            separator = ", ";
-
-        if (MaybeAddModification(repo, separator, ref result))
-            separator = ", ";
-
-        MaybeAddRenaming(repo, separator, ref result);
+        foreach (var heuristic in heuristics)
+            separator = MaybeAddHeuristic(repo, separator, heuristic, ref result) ?? separator;
 
         return result;
     }
 
-    private bool MaybeAddTicket(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new TicketHeuristic(), ref commitMessage);
-
-    private bool MaybeAddSubject(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new SubjectHeuristic(), ref commitMessage);
-
-    private bool MaybeAddCreation(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new CreationHeuristic(), ref commitMessage);
-
-    private bool MaybeAddDeletion(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new DeletionHeuristic(), ref commitMessage);
-
-    private bool MaybeAddModification(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new ModificationHeuristic(), ref commitMessage);
-
-    private bool MaybeAddRenaming(Repository repo, string before, ref string commitMessage) =>
-        MaybeAddHeuristic(repo, before, new RenamingHeuristic(), ref commitMessage);
-
-    private bool MaybeAddHeuristic(Repository repo, string before, IHeuristic heuristic, ref string commitMessage) {
+    private string? MaybeAddHeuristic(Repository repo, string before, IHeuristic heuristic, ref string commitMessage) {
         var heuristicResult = heuristic.Analyse(repo);
 
         var shouldIncludeHeuristic = heuristicResult.Priority > 0 &&
@@ -57,6 +35,6 @@ public class CommitMessageAssembler {
         if (shouldIncludeHeuristic)
             commitMessage += $"{before}{heuristicResult.Value}";
 
-        return shouldIncludeHeuristic;
+        return heuristicResult.After;
     }
 }
