caddeccb7422a171eeae93e2cc76b56848fadc79
Joshua Hull <josh.hull.w@gmail.com>
2022-11-16 23:48:14 +1100

    feat(web-1234): updated ticket-heuristic

--- src/ShutUpHusky/Heuristics/TicketHeuristic.cs
+++ src/ShutUpHusky/Heuristics/TicketHeuristic.cs
@@ -1,9 +1,18 @@
+using System.Text.RegularExpressions;
 using LibGit2Sharp;
 
 namespace ShutUpHusky.Heuristics;
-
+ 
 public class TicketHeuristic : IHeuristic {
     public HeuristicResult Analyse(IRepository repo) {
+        var ticketNumberMatch = Regex.Match(repo.Head.FriendlyName, "[a-zA-Z]+\\-[0-9]+");
+
+        if (ticketNumberMatch.Success)
+            return new() {
+                Priority = 10,
+                Value = $"feat({ticketNumberMatch.Value.ToLowerInvariant()})",
+            };
+
         return new() {
             Priority = 10,
             Value = $"feat(rand-{(Math.Floor(new Random().NextDouble() * 9999))})",
