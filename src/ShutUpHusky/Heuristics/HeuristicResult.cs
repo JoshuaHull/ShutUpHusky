69fb2649b24b9612345905b0da5f792b905fc35c
Joshua Hull <josh.hull.w@gmail.com>
2022-11-26 16:51:31 +1100

    feat(rand-8217): updated heuristic-result

--- src/ShutUpHusky/Heuristics/HeuristicResult.cs
+++ src/ShutUpHusky/Heuristics/HeuristicResult.cs
@@ -1,7 +1,7 @@
 namespace ShutUpHusky.Heuristics;
 
 internal record HeuristicResult {
-    public required int Priority { get; init; }
+    public required double Priority { get; init; }
     public required string Value { get; init; } = string.Empty;
     public string? After { get; init; } = null;
 }
