2f8f1f392bf678b142e5d26b14126f4b2941d500
Joshua Hull <josh.hull.w@gmail.com>
2022-11-26 18:21:44 +1100

    feat: extensions > updated lib-git2sharp-extensions

--- src/ShutUpHusky/Utils/FilePathExtensions.cs
+++ src/ShutUpHusky/Utils/FilePathExtensions.cs
@@ -7,6 +7,15 @@ internal static class FilePathExtensions {
         if (fullFileName.StartsWith("."))
             return fullFileName;
 
-        return fullFileName.Split(".").First();
+        return fullFileName.Split(".").FirstOrDefault() ?? string.Empty;
+    }
+
+    public static string GetFileExtension(this string filePath) {
+        var fullFileName = filePath.Split("/").Last();
+
+        if (fullFileName.StartsWith("."))
+            fullFileName = fullFileName[1..];
+
+        return fullFileName.Split(".").LastOrDefault() ?? string.Empty;
     }
 }
--- src/ShutUpHusky/Utils/LibGit2SharpExtensions.cs
+++ src/ShutUpHusky/Utils/LibGit2SharpExtensions.cs
@@ -17,15 +17,72 @@ internal static class LibGit2SharpExtensions {
         .OrderByDescending(eval)
         .ToList();
 
+    /* IRepository extensions */
+
+    public static List<StatusEntry> GetAllAlteredFiles(this IRepository repo) =>
+        repo
+            .RetrieveStatus(new StatusOptions())
+            .GetAllAlteredFiles();
+
     public static List<StatusEntry> GetModifiedFiles(this IRepository repo) =>
         repo
             .RetrieveStatus(new StatusOptions())
             .GetModifiedFiles();
 
-    public static List<StatusEntry> GetModifiedFiles(this RepositoryStatus status) =>
+    public static List<StatusEntry> GetDeletedFiles(this IRepository repo) =>
+        repo
+            .RetrieveStatus(new StatusOptions())
+            .GetDeletedFiles();
+
+    /* RepositoryStatus extensions */
+
+    public static List<StatusEntry> GetFiles(this RepositoryStatus status, params FileStatus[] statuses) =>
         status
-            .Where(file => file.State == FileStatus.ModifiedInIndex)
+            .Where(file => statuses.Contains(file.State))
             .ToList();
+
+    public static List<StatusEntry> GetAllAlteredFiles(this RepositoryStatus status) =>
+        status.GetFiles(FileStatus.ModifiedInIndex, FileStatus.NewInIndex, FileStatus.DeletedFromIndex, FileStatus.RenamedInIndex);
+
+    public static List<StatusEntry> GetModifiedFiles(this RepositoryStatus status) =>
+        status.GetFiles(FileStatus.ModifiedInIndex);
+
+    public static List<StatusEntry> GetDeletedFiles(this RepositoryStatus status) =>
+        status.GetFiles(FileStatus.DeletedFromIndex);
+
+    /* List<StatusEntry> Extensions */
+
+    public static List<StatusEntry> WithFileExtensions(this List<StatusEntry> entries, params string[] extensions) =>
+        entries
+            .Where(entry => extensions.Contains(entry.GetFileExtension().ToLowerInvariant()))
+            .ToList();
+
+    public static IEnumerable<string> ToFileTerms(this List<StatusEntry> entries) =>
+        entries
+            .Select(entry => entry.GetFileName().CamelCaseToKebabCase())
+            .SelectMany(n => n.Split("-"));
+
+    public static List<string> WithFileTerms(this List<StatusEntry> entries, params string[] terms) =>
+        entries
+            .ToFileTerms()
+            .Where(term => terms.Contains(term))
+            .ToList();
+
+    public static List<StatusEntry> GetFiles(this List<StatusEntry> entries, params FileStatus[] statuses) =>
+        entries
+            .Where(entry => statuses.Contains(entry.State))
+            .ToList();
+
+    public static List<StatusEntry> GetDeletedFiles(this List<StatusEntry> entries) =>
+        entries.GetFiles(FileStatus.DeletedFromIndex);
+
+    /* StatusEntry Extensions */
+
+    public static string GetFileExtension(this StatusEntry entry) =>
+        entry.FilePath.GetFileExtension();
+
+    public static string GetFileName(this StatusEntry entry) =>
+        entry.FilePath.GetFileName();
 
     public static string ToUpdatedCommitMessageSnippet(this StatusEntry statusEntry) =>
         $"updated {statusEntry.FilePath.GetFileName().CamelCaseToKebabCase()}";
