284b9c469436deea4de3df88f80670ef1e61aca5
Joshua Hull <josh.hull.w@gmail.com>
2022-11-26 18:21:52 +1100

    feat: updated constants

--- src/ShutUpHusky/Constants.cs
+++ src/ShutUpHusky/Constants.cs
@@ -6,4 +6,34 @@ internal static class Constants {
     public const int LowPriorty = 1;
     public const int NotAPriority = 0;
     public const int MaxCommitTitleLength = 72;
+    public const double TypeOverrideThreshold = 0.8;
+
+    public static class FileExtensions {
+        public const string Yaml = "yaml";
+        public const string Md = "md";
+    }
+
+    public static class Types {
+        public const string Feat = "feat";
+        public const string Fix = "fix";
+        public const string Perf = "perf";
+        public const string Ci = "ci";
+        public const string Test = "test";
+        public const string Chore = "chore";
+        public const string Docs = "docs";
+        public const string MatchAny = "fix|ci|perf|feat|docs|test|chore";
+    }
+
+    public static class Terms {
+        public const string Test = "test";
+        public const string Tests = "tests";
+        public const string Spec = "spec";
+        public const string Specs = "specs";
+        public static string[] All => new[] {
+            Test,
+            Tests,
+            Spec,
+            Specs,
+        };
+    }
 }
