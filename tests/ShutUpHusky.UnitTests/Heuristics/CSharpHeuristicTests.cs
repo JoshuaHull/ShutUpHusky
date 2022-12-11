31043efe200f7f1e425ea97ac95ff3c3623defc2
Joshua Hull <josh.hull.w@gmail.com>
2022-12-11 19:24:43 +1100

    test: updated csharp-heuristic-tests, added + After = , ,, added + +

--- tests/ShutUpHusky.UnitTests/Heuristics/CSharpHeuristicTests.cs
+++ tests/ShutUpHusky.UnitTests/Heuristics/CSharpHeuristicTests.cs
@@ -61,17 +61,17 @@ public class CSharpHeuristicTests
         // Assert
         result.Should().BeEquivalentTo(new HeuristicResult[] {
             new() {
-                Priority = 0.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 3),
+                Priority = 0.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 3),
                 Value = "replaced secondReplacedLine new SomeObject 11 with unrelatedArray new[] 1, 2, 3",
                 After = ", ",
             },
             new() {
-                Priority = 1.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 3),
+                Priority = 1.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 3),
                 Value = "replaced wow with wew",
                 After = ", ",
             },
             new() {
-                Priority = 2.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 3),
+                Priority = 2.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 3),
                 Value = "added string wowStatement = wowow",
                 After = ", ",
             },
@@ -128,30 +128,187 @@ public class CSharpHeuristicTests
         // Assert
         result.Should().BeEquivalentTo(new HeuristicResult[] {
             new() {
-                Priority = 0.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 5),
+                Priority = 0.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
                 Value = "added var trimmed = snippet Trim",
                 After = ", ",
             },
             new() {
-                Priority = 1.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 5),
+                Priority = 1.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
                 Value = "added AddSnippet string snippet",
                 After = ", ",
             },
             new() {
-                Priority = 2.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 5),
+                Priority = 2.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
                 Value = "added class CommitMessage",
                 After = ", ",
             },
             new() {
-                Priority = 3.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 5),
+                Priority = 3.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
                 Value = "added string Value",
                 After = ", ",
             },
             new() {
-                Priority = 4.ToPriority(Constants.LowPriorty, Constants.LanguageSpecificPriority, 5),
+                Priority = 4.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
                 Value = "added double Evaluate",
                 After = ", ",
             },
         }).And.BeInDescendingOrder(r => r.Priority);
+    }
+
+    [TestCase(
+    """
+    diff --git a/src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs b/src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs
+    index 03e3916..02cd15e 100644
+    --- a/src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs
+    +++ b/src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs
+    @@ -33,7 +33,7 @@ internal class TypescriptHeuristic : IHeuristic {
+             var tokenizer = new Tokenizer(new() {
+                 SplitRegex =
+                     \"\"\"
+    -                [\\.\\(\\)\\s'\";:{}]
+    +                [\\.\\(\\)\\s'\";:{}]|\\bpublic\\b|\\bprivate\\b|\\bget\\b|\\bset\\b|\\bthis\\b|\\babstract\\b|\\bbase\\b|\\bvoid\\b
+                     \"\"\",
+             });
+             var tokenized = tokenizer.Tokenize(changedLines).ToList();
+    @@ -45,6 +45,9 @@ internal class TypescriptHeuristic : IHeuristic {
+                     \"typeof\", \"delete\", \"enum\", \"true\", \"false\", \"in\", \"null\", \"undefined\", \"with\", \"satisfies\", \"as\", \"public\", \"private\",
+                     \"implements\", \"extends\", \"package\", \"static\", \"protected\", \"yield\", \"=\", \"==\", \"===\"
+                 },
+    +            IgnoreLinesStartingWithToken = new[] {
+    +                \"export\", \"import\"
+    +            },
+             });
+             scorboard.AddAll(tokenized);
+     
+    """,
+    TestName = "TypescriptHeuristic.cs",
+    ExpectedResult = "replaced ] with ]| bpublic b| bprivate bget bset bthis babstract bbase bvoid b"
+    )]
+    [TestCase(
+    """
+    diff --git a/tests/ShutUpHusky.UnitTests/Heuristics/TypescriptHeuristicTests.cs b/tests/ShutUpHusky.UnitTests/Heuristics/TypescriptHeuristicTests.cs
+    index 97dc7be..b7340df 100644
+    --- a/tests/ShutUpHusky.UnitTests/Heuristics/TypescriptHeuristicTests.cs
+    +++ b/tests/ShutUpHusky.UnitTests/Heuristics/TypescriptHeuristicTests.cs
+    @@ -75,6 +75,83 @@ public class TypescriptHeuristicTests
+                     Value = \"added const wowStatement = wowow\",
+                     After = \", \",
+                 },
+    -        });
+    +        }).And.BeInDescendingOrder(r => r.Priority);
+    +    }
+    +
+    +    [Test]
+    +    public void ShouldWriteMeaningfulCommitMessage_FromAddedProperties_AndFunctions_AndClasses_Etc() {
+    +        // Arrange
+    +        var newTypescriptFile = new MockPatch {
+    +            Content =
+    +                \"\"\"
+    +                diff --git a/src/CommitMessage.ts b/src/CommitMessage.ts
+    +                index 811984f..b4d43d5 100644
+    +                --- /dev/null
+    +                +++ b/src/CommitMessage.ts
+    +                @@ -3,5 +3,7 @@+import { git } from './command-line';
+    +                +public class CommitMessage implements ICommitMessage {
+    +                +
+    +                +    public value: string;
+    +                +
+    +                +    public addSnippet(snippet: string): void {
+    +                +        const trimmed = snippet.trim();
+    +                +    }
+    +                +
+    +                +    evaluate: () => double = () => value.length / 3.00;
+    +                +}
+    +                \"\"\",
+    +        };
+    +
+    +        var repo = new MockRepository {
+    +            Head = new MockBranch {
+    +                Tip = new MockCommit {
+    +                    Tree = new MockTree {
+    +                    }.Object,
+    +                }.Object,
+    +            }.Object,
+    +            Status = new MockRepositoryStatus {
+    +                StatusEntries = new[] {
+    +                    new MockStatusEntry {
+    +                        State = FileStatus.NewInIndex,
+    +                        FilePath = \"src/CommitMessage.ts\",
+    +                    }.Object,
+    +                },
+    +            }.Object,
+    +            Diff = new MockDiff()
+    +                .SeedPatch(\"src/CommitMessage.ts\", newTypescriptFile.Object)
+    +                .Object,
+    +        };
+    +
+    +        // Act
+    +        var result = Heuristic.Analyse(repo.Object);
+    +
+    +        // Assert
+    +        result.Should().BeEquivalentTo(new HeuristicResult[] {
+    +            new() {
+    +                Priority = 0.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
+    +                Value = \"added evaluate => double = => value length / 3 00\",
+    +                After = \", \",
+    +            },
+    +            new() {
+    +                Priority = 1.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
+    +                Value = \"added addSnippet snippet string\",
+    +                After = \", \",
+    +            },
+    +            new() {
+    +                Priority = 2.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
+    +                Value = \"added value string\",
+    +                After = \", \",
+    +            },
+    +            new() {
+    +                Priority = 3.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
+    +                Value = \"added const trimmed = snippet trim\",
+    +                After = \", \",
+    +            },
+    +            new() {
+    +                Priority = 4.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, 5),
+    +                Value = \"added class CommitMessage implements ICommitMessage\",
+    +                After = \", \",
+    +            },
+    +        }).And.BeInDescendingOrder(r => r.Priority);
+         }
+     }
+    """,
+    TestName = "TypescriptHeuristicTests.cs",
+    ExpectedResult = "added Priority = 3 ToPriority Constants LowPriority, Constants LanguageSpecificPriority, 5 ,"
+    )]
+    public string ShouldWriteMeaningfulCommitMessages_ForRealCommitDiffs(string patchContent) {
+        // Arrange
+        var newCSharpFile = new MockPatch {
+            Content = patchContent,
+        };
+
+        var repo = new MockRepository {
+            Head = new MockBranch {
+                Tip = new MockCommit {
+                    Tree = new MockTree {
+                    }.Object,
+                }.Object,
+            }.Object,
+            Status = new MockRepositoryStatus {
+                StatusEntries = new[] {
+                    new MockStatusEntry {
+                        State = FileStatus.NewInIndex,
+                        FilePath = "src/CommitMessage.cs",
+                    }.Object,
+                },
+            }.Object,
+            Diff = new MockDiff()
+                .SeedPatch("src/CommitMessage.cs", newCSharpFile.Object)
+                .Object,
+        };
+
+        // Act
+        var result = Heuristic.Analyse(repo.Object);
+
+        // Assert
+        return result.First().Value;
     }
 }
