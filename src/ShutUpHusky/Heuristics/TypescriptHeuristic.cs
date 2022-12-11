5177d04669f0b3822deac2444316f82f7a3c1645
Joshua Hull <josh.hull.w@gmail.com>
2022-12-11 22:12:32 +1100

    chore: updated typescript-heuristic, deleted var files = repo

--- src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs
+++ src/ShutUpHusky/Heuristics/TypescriptHeuristic.cs
@@ -9,64 +9,61 @@ namespace ShutUpHusky.Heuristics;
 // https://arxiv.org/pdf/1805.03989.pdf
 // https://github.com/lancopku/Global-Encoding
 internal class TypescriptHeuristic : IHeuristic {
+    private readonly string[] TokenScoreboardIgnoredTokens = new[] {
+        "const", "import", "export", "interface", "class", "type", "var", "function", "let", "break", "continue", "debugger",
+        "do", "try", "finally", "catch", "instanceof", "return", "void", "case", "default", "if", "for", "else", "while",
+        "typeof", "delete", "enum", "true", "false", "in", "null", "undefined", "with", "satisfies", "as", "public", "private",
+        "implements", "extends", "package", "static", "protected", "yield", "=", "==", "===", "[", "]", "|", ",", "+", "-",
+    };
+
+    private readonly string[] TokenScoreboardIgnoreLinesStartingWith = new[] {
+        "export", "import"
+    };
+
+    private readonly string TokenizerSplitRegex =
+        """
+        [\\\.\(\)\s'";:{}]|\bpublic\b|\bprivate\b|\bget\b|\bset\b|\bthis\b|\babstract\b|\bbase\b|\bvoid\b
+        """;
+
     public ICollection<HeuristicResult> Analyse(IRepository repo) {
-        var files = repo
-            .GetFiles(FileStatus.ModifiedInIndex, FileStatus.NewInIndex, FileStatus.DeletedFromIndex)
-            .ToList();
-
-        if (files.Count == 0)
-            return Array.Empty<HeuristicResult>();
-
+        var files = repo.GetFiles(FileStatus.ModifiedInIndex, FileStatus.NewInIndex, FileStatus.DeletedFromIndex);
         var typescriptExtensions = new[] { "ts", "tsx" };
-        var typescriptFiles = files
-            .Where(file => typescriptExtensions.Contains(file.GetFileExtension()))
-            .ToList();
+        var typescriptFiles = files.Where(file => typescriptExtensions.Contains(file.GetFileExtension()));
 
-        if (typescriptFiles.Count == 0)
-            return Array.Empty<HeuristicResult>();
+        return typescriptFiles
+            .Select(file => file.ToPatch(repo))
+            .SelectMany(AnalyseTypescriptFile)
+            .ToArray();
+    }
 
-        var statusEntriesByPatch = typescriptFiles.MapPatchToStatusEntry(repo);
-
+    private IEnumerable<HeuristicResult> AnalyseTypescriptFile(Patch typescriptFile) {
         var patchParser = new PatchParser();
-        var changedLines = patchParser.Parse(statusEntriesByPatch.Keys.First());
+        var changedLines = patchParser.Parse(typescriptFile);
 
         var tokenizer = new Tokenizer(new() {
-            SplitRegex =
-                """
-                [\\\.\(\)\s'";:{}]|\bpublic\b|\bprivate\b|\bget\b|\bset\b|\bthis\b|\babstract\b|\bbase\b|\bvoid\b
-                """,
+            SplitRegex = TokenizerSplitRegex,
         });
         var tokenized = tokenizer.Tokenize(changedLines).ToList();
 
         var scorboard = new TokenScoreboard(new() {
-            IgnoredTokens = new[] {
-                "const", "import", "export", "interface", "class", "type", "var", "function", "let", "break", "continue", "debugger",
-                "do", "try", "finally", "catch", "instanceof", "return", "void", "case", "default", "if", "for", "else", "while",
-                "typeof", "delete", "enum", "true", "false", "in", "null", "undefined", "with", "satisfies", "as", "public", "private",
-                "implements", "extends", "package", "static", "protected", "yield", "=", "==", "===", "[", "]", "|", ",", "+", "-",
-            },
-            IgnoreLinesStartingWithToken = new[] {
-                "export", "import"
-            },
+            IgnoredTokens = TokenScoreboardIgnoredTokens,
+            IgnoreLinesStartingWithToken = TokenScoreboardIgnoreLinesStartingWith,
         });
         scorboard.AddAll(tokenized);
 
         var highestScoringLines = scorboard.HighestScoringLines().ToArray();
         var lineCount = highestScoringLines.Length;
-        var rtn = new HeuristicResult[lineCount];
 
         for (var i = 0; i < lineCount; i += 1) {
             var line = highestScoringLines[i];
             var commitMessageSnippet = line.ToString();
             var priority = i.ToPriority(Constants.LowPriority, Constants.LanguageSpecificPriority, lineCount);
 
-            rtn[i] = new() {
+            yield return new() {
                 Priority = priority,
                 Value = commitMessageSnippet,
                 After = ", ",
             };
         }
-
-        return rtn;
     }
 }
