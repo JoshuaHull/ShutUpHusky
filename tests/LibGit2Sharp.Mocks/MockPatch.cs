1e93ca1db74cb8971cb9e020edbc1eab228555d3
Joshua Hull <josh.hull.w@gmail.com>
2022-12-11 13:06:08 +1100

    chore: updated mock-patch, added string? Content, added using System

--- tests/LibGit2Sharp.Mocks/MockPatch.cs
+++ tests/LibGit2Sharp.Mocks/MockPatch.cs
@@ -1,3 +1,4 @@
+using System;
 using Moq;
 
 namespace LibGit2Sharp.Mocks;
@@ -5,9 +6,11 @@ namespace LibGit2Sharp.Mocks;
 public class MockPatch: Mock<Patch> {
     public int LinesAdded { get; set; }
     public int LinesDeleted { get; set; }
+    public string? Content { get; set; }
 
     public MockPatch() {
         SetupGet(p => p.LinesAdded).Returns(() => LinesAdded);
         SetupGet(p => p.LinesDeleted).Returns(() => LinesDeleted);
+        SetupGet(p => p.Content).Returns(() => Content ?? throw new NullReferenceException());
     }
 }
