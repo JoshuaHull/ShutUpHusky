55aa815d26ea53fa5f2c2f611cd0074eb9b4ae3e
Joshua Hull <josh.hull.w@gmail.com>
2022-12-13 23:01:28 +1100

    chore: updated mock-diff, replaced Patch> with MockPatch>

--- tests/LibGit2Sharp.Mocks/MockDiff.cs
+++ tests/LibGit2Sharp.Mocks/MockDiff.cs
@@ -5,17 +5,17 @@ using Moq;
 namespace LibGit2Sharp.Mocks;
 
 public class MockDiff: Mock<Diff> {
-    private readonly Dictionary<string, Patch> _patches = new();
+    private readonly Dictionary<string, MockPatch> _patches = new();
 
     public MockDiff() {
         Setup(d => d.Compare<Patch>(It.IsAny<IEnumerable<string>>()))
-            .Returns<IEnumerable<string>>(paths => _patches[paths.Single()]);
+            .Returns<IEnumerable<string>>(paths => _patches[paths.Single()].Object);
 
         Setup(d => d.Compare<Patch>(It.IsAny<Tree>(), It.IsAny<DiffTargets>(), It.IsAny<IEnumerable<string>>()))
-            .Returns<Tree, DiffTargets, IEnumerable<string>>((tree, targets, paths) => _patches[paths.Single()]);
+            .Returns<Tree, DiffTargets, IEnumerable<string>>((tree, targets, paths) => _patches[paths.Single()].Object);
     }
 
-    public MockDiff SeedPatch(string filePath, Patch patch) {
+    public MockDiff SeedPatch(string filePath, MockPatch patch) {
         _patches.Add(filePath, patch);
         return this;
     }
