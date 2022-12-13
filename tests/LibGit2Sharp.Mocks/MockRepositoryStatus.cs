0e54a18c3599626674d75c6c843e7505a7e688df
Joshua Hull <josh.hull.w@gmail.com>
2022-12-13 23:01:34 +1100

    chore: updated mock-repository-status, added => _staged = value ToList

--- tests/LibGit2Sharp.Mocks/MockRepositoryStatus.cs
+++ tests/LibGit2Sharp.Mocks/MockRepositoryStatus.cs
@@ -1,15 +1,29 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Moq;
 
 namespace LibGit2Sharp.Mocks;
 
 public class MockRepositoryStatus: Mock<RepositoryStatus> {
-    public IEnumerable<StatusEntry> Staged { get; set; } = Array.Empty<StatusEntry>();
-    public IEnumerable<StatusEntry> StatusEntries { get; set; } = Array.Empty<StatusEntry>();
+    private List<MockStatusEntry> _staged = new();
+    public IEnumerable<MockStatusEntry> Staged {
+        get => _staged;
+        set => _staged = value.ToList();
+    }
+
+    private List<MockStatusEntry> _statusEntries = new();
+    public IEnumerable<MockStatusEntry> StatusEntries {
+        get => _statusEntries;
+        set => _statusEntries = value.ToList();
+    }
 
     public MockRepositoryStatus() {
-        SetupGet(e => e.Staged).Returns(() => Staged);
-        Setup(s => s.GetEnumerator()).Returns(() => StatusEntries.GetEnumerator());
+        SetupGet(e => e.Staged).Returns(() => Staged.Select(s => s.Object));
+        Setup(s => s.GetEnumerator()).Returns(() => StatusEntries.Select(e => e.Object).GetEnumerator());
+    }
+
+    public MockRepositoryStatus SeedStatusEntry(MockStatusEntry entry) {
+        _statusEntries.Add(entry);
+        return this;
     }
 }
