ed50824598aa2b534129fe02d045659e7b805f22
Joshua Hull <josh.hull.w@gmail.com>
2022-12-17 19:33:13 +1100

    test: updated commit-message-assembler tests - added + string Value

--- tests/ShutUpHusky.UnitTests/CommitMessageAssemblerTests.cs
+++ tests/ShutUpHusky.UnitTests/CommitMessageAssemblerTests.cs
@@ -167,4 +167,90 @@ public class CommitMessageAssemblerTests {
             """
         );
     }
+
+    [TestCase(
+    """
+    diff --git a/src/ShutUpHusky/CommitMessageAssembler.cs b/src/ShutUpHusky/CommitMessageAssembler.cs
+    index a0f2b51..8aa3fbc 100644
+    --- a/src/ShutUpHusky/CommitMessageAssembler.cs
+    +++ b/src/ShutUpHusky/CommitMessageAssembler.cs
+    @@ -63,14 +63,20 @@ public class CommitMessageAssembler {
+     
+             var (h, hs) = (r[0], r[1..]);
+     
+    -        var canAddSnippetToTitle = commitMessage.CanAddSnippetToTitle(h.Value);
+    -        var canAddSnippetToBody = !canAddSnippetToTitle && _options.EnableBody;
+    -
+             var nextHeuristics = rs.Append(hs).ToArray();
+     
+             if (h.Priority == Constants.NotAPriority)
+                 return ApplyHeuristics(commitMessage, repo, nextHeuristics);
+     
+    +        var canAddSnippetToTitle = commitMessage.CanAddSnippetToTitle(h.Value);
+    +        var canAddSnippetToBody = !canAddSnippetToTitle && _options.EnableBody;
+    +
+    +        if (!canAddSnippetToTitle && h.Shortened is not null) {
+    +            var hsWithShortened = hs.Append(h.Shortened).OrderByDescending(r => r.Priority).ToArray();
+    +            var nextHeuristicsWithShortened = rs.Append(hsWithShortened).ToArray();
+    +            return ApplyHeuristics(commitMessage, repo, nextHeuristicsWithShortened);
+    +        }
+    +
+             if (canAddSnippetToTitle)
+                 return ApplyHeuristics(commitMessage.AddMessageSnippetToTitle(h.Value).WithNextSeparator(h.After ?? string.Empty), repo, nextHeuristics);
+     
+    """,
+    "src/ShutUpHusky/CommitMessageAssembler.cs",
+    FileStatus.ModifiedInIndex,
+    ExpectedResult = "chore: updated commit-message-assembler"
+    )]
+    [TestCase(
+    """
+    diff --git a/files/CreatedFile.cs b/files/CreatedFile.cs
+    index 811984f..b4d43d5 100644
+    --- /dev/null
+    +++ b/files/CreatedFile.cs
+    @@ -3,5 +3,7 @@+namespace ShutUpHusky.Commits;
+    +public abstract class CommitMessage {
+    +
+    +    public string Value { get; private set; }
+    +
+    +    public void AddSnippet(string snippet) {
+    +        var trimmed = snippet.Trim();
+    +    }
+    +
+    +    protected abstract double Evaluate();
+    +}
+    """,
+    "files/CreatedFile.cs",
+    FileStatus.NewInIndex,
+    ExpectedResult = "chore: created created-file - added var trimmed = snippet Trim"
+    )]
+    public string ShouldCreateCommitMessage_WithASummary_WhenSummariesAreEnabled(string patchContent, string filePath, FileStatus state) {
+        // Arrange
+        var repo = new MockRepository {
+            Head = new MockBranch {
+                FriendlyName = "main",
+            }
+            .WithSensibleDefaults(),
+        }
+        .WithSensibleDefaults()
+        .SeedPatch(
+            new MockStatusEntry {
+                State = state,
+                FilePath = filePath,
+            },
+            new MockPatch {
+                LinesAdded = 100,
+                LinesDeleted = 0,
+                Content = patchContent,
+            }
+        );
+
+        var assembler = new CommitMessageAssembler(new() {
+            EnableSummaries = true,
+        });
+
+        // Act & Assert
+        return assembler.Assemble(repo.Object);
+    }
 }
