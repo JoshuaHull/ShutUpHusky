51a8c54b97c08ab77e7cd00fd7305faee9214484
Joshua Hull <josh.hull.w@gmail.com>
2022-12-17 19:32:46 +1100

    chore: updated deletion-heuristic - added var patch = file ToPatch repo

--- src/ShutUpHusky/Heuristics/FileHeuristics/DeletionHeuristic.cs
+++ src/ShutUpHusky/Heuristics/FileHeuristics/DeletionHeuristic.cs
@@ -1,18 +1,41 @@
 using LibGit2Sharp;
+using ShutUpHusky.Analysis.Summarizers;
 using ShutUpHusky.Files;
 using ShutUpHusky.Utils;
 
 namespace ShutUpHusky.Heuristics.FileHeuristics;
 
 internal class DeletionHeuristic : IFileHeuristic {
+    public FileHeuristicOptions? Options { private get; init; }
+
     public HeuristicResult[] Analyse(IRepository repo) {
         var deletedFiles = repo.GetDeletedFiles();
 
         return deletedFiles
-            .Select(file => new HeuristicResult {
-                Priority = Constants.LowPriority + file.ToPatch(repo).LinesDeleted,
-                Value = file.ToCommitMessageSnippet(FileChangeType.Deleted),
-                After = ", ",
+            .Select(file => {
+                var patch = file.ToPatch(repo);
+                var fileChangeSnippet = file.ToCommitMessageSnippet(FileChangeType.Deleted);
+                var includeDiffSummary = Options?.IncludeDiffSummary is true;
+                var summarizer = includeDiffSummary ? SummarizerFactory.Create(file) : null;
+                var summary = summarizer?.Summarize(patch);
+
+                if (summary is null)
+                    return new HeuristicResult {
+                        Priority = Constants.LowPriority + patch.LinesDeleted,
+                        Value = fileChangeSnippet,
+                        After = ", ",
+                    };
+
+                var prefixedSummary = HeuristicResult.PrefixAll(summary, $"{fileChangeSnippet} - ");
+
+                return HeuristicResult.AddShortened(
+                    prefixedSummary,
+                    new() {
+                        Priority = Constants.LowPriority + patch.LinesDeleted,
+                        Value = fileChangeSnippet,
+                        After = ", ",
+                    }
+                );
             })
             .OrderByDescending(_ => _.Priority)
             .ToArray();
