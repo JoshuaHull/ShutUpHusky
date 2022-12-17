8a4474f4f5e12f5f66b8455d224f59750d3d3ceb
Joshua Hull <josh.hull.w@gmail.com>
2022-12-17 19:32:49 +1100

    chore: updated modification-heuristic - added Value = fileChangeSnippet,

--- src/ShutUpHusky/Heuristics/FileHeuristics/ModificationHeuristic.cs
+++ src/ShutUpHusky/Heuristics/FileHeuristics/ModificationHeuristic.cs
@@ -1,22 +1,41 @@
 using LibGit2Sharp;
+using ShutUpHusky.Analysis.Summarizers;
 using ShutUpHusky.Files;
 using ShutUpHusky.Utils;
 
 namespace ShutUpHusky.Heuristics.FileHeuristics;
 
 internal class ModificationHeuristic : IFileHeuristic {
+    public FileHeuristicOptions? Options { private get; init; }
+
     public HeuristicResult[] Analyse(IRepository repo) {
         var modifiedFiles = repo.GetModifiedFiles();
 
         return modifiedFiles
             .Select(file => {
                 var patch = file.ToPatch(repo);
+                var fileChangeSnippet = file.ToCommitMessageSnippet(FileChangeType.Modified);
+                var includeDiffSummary = Options?.IncludeDiffSummary is true;
+                var summarizer = includeDiffSummary ? SummarizerFactory.Create(file) : null;
+                var summary = summarizer?.Summarize(patch);
 
-                return new HeuristicResult {
-                    Priority = Constants.LowPriority + patch.LinesAdded + patch.LinesDeleted,
-                    Value = file.ToCommitMessageSnippet(FileChangeType.Modified),
-                    After = ", ",
-                };
+                if (summary is null)
+                    return new HeuristicResult {
+                        Priority = Constants.LowPriority + patch.LinesAdded + patch.LinesDeleted,
+                        Value = fileChangeSnippet,
+                        After = ", ",
+                    };
+
+                var prefixedSummary = HeuristicResult.PrefixAll(summary, $"{fileChangeSnippet} - ");
+
+                return HeuristicResult.AddShortened(
+                    prefixedSummary,
+                    new() {
+                        Priority = Constants.LowPriority + patch.LinesAdded + patch.LinesDeleted,
+                        Value = fileChangeSnippet,
+                        After = ", ",
+                    }
+                );
             })
             .OrderByDescending(_ => _.Priority)
             .ToArray();
